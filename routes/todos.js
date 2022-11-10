615c796a32778c2fba0a3bbcc61a7bdd0449c522
darrielcodes <darriel.morris@codeimmersives.comgit config --global user.email darrielmorris@yahoo.com>
2022-11-10 12:20:43 -0800

    Completed assignment and first stretch goal

---
+++ routes/todos.js
@@ -0,0 +1,210 @@
+var express = require('express');
+var router = express.Router();
+const { uuid } = require("uuidv4")
+const { db } = require("../mongo");
+
+const mockTodos = [{
+	id: "4387f4d8-aeac-4559-9f1b-3c5d537c955c",
+	title: "Implement Fullstack ToDo List",
+	description: "Implement the fullstack todo list application.",
+	isComplete: false,
+	priority: "High",
+	creationDate: new Date(),
+	lastModified: new Date(),
+	completedDate: null
+}, {
+	id: "e365f13c-4c1d-4ee1-8a66-3dbbbab71f0d",
+	title: "Create /all route for mock data",
+	description: "Create an express route that will respond with the mock todo list.",
+	isComplete: false,
+	priority: "High",
+	creationDate: new Date(),
+	lastModified: new Date(),
+	completedDate: null
+}, {
+	id: "08dd1f20-7d31-4120-89ed-343d4006a7cb",
+	title: "Create a home page in the client",
+	description: "Create a Home Page in React that will display all the todos.",
+	isComplete: false,
+	priority: "High",
+	creationDate: new Date(),
+	lastModified: new Date(),
+	completedDate: null
+}, {
+	id: "98a06f8f-50c9-4832-9d2d-daa45543db00",
+	title: "Create the todo card component",
+	description: "Create a react ToDoCard component that will be rendered for each todo on the home page.",
+	isComplete: false,
+	priority: "Medium",
+	creationDate: new Date(),
+	lastModified: new Date(),
+	completedDate: null
+}, {
+	id: "7c5d70bb-2a00-4009-9bb8-1bb163fb501f",
+	title: "Test basic application with mock data",
+	description: "Visit the client Home Page to see the todo's displayed as a list.",
+	isComplete: false,
+	priority: "Medium",
+	creationDate: new Date(),
+	lastModified: new Date(),
+	completedDate: null
+}];
+
+/* GET all */
+router.get('/all', async function(req, res, next) {
+  try {
+      const toDoList = await db().collection("toDos").find({}).toArray()
+    res.json({
+      success: true,
+      toDos: toDoList
+  })
+} catch (err) {
+        console.error(err);
+            res.json({
+          success: false,
+          error: err.toString()
+        });
+      }
+});
+
+/* POST */ 
+router.post('/create-one', async (req, res) => {
+    try {
+        const title = req.body.title;
+        const description = req.body.description;
+        const priority = req.body.priority;
+
+        const newToDo = {
+            title: title,
+            description: description,
+            priority: priority,
+            id: uuid(),
+            isComplete: false,
+            creationDate: new Date(),
+            lastModified: new Date(),
+            completedDate: null
+        };
+        /* VALIDATION CHECK */
+        const validateData = (newToDo) => {
+            console.log(newToDo.title.length)
+            if (newToDo.title === undefined || newToDo.description === undefined){
+                return {
+                    success: false,
+                    message: "Title and description must be defined."
+                }
+            }
+            if(newToDo.title.length === 0 || newToDo.description.length === 0) {
+                return {
+                    success: false,
+                    message: "Title and description must be atleast one character."
+                }
+            }
+            if(typeof(newToDo.title) !== "string" || typeof(newToDo.description) !== "string") {
+                return {
+                    success: false,
+                    message: "Title and description must be strings."
+                }
+            }
+            if(newToDo.priority !== "High" && newToDo.priority !== "Medium" && newToDo.priority !== "Low") {
+                return {
+                    success: false,
+                    message: "Priority must be 'High', 'Medium', or 'Low'."
+                }
+            }
+             else {
+                return {
+                    success: true
+                }
+            }
+        };
+        
+       
+       const validationResult = validateData(newToDo)
+       if (validationResult.success === false) {
+           console.log(validationResult)
+        res.json(validationResult)
+        return
+       };
+
+        const toDos = await db().collection("toDos").insertOne(newToDo);
+
+        res.json ({
+            success: true,
+            newToDo: newToDo
+          });
+    } catch(err) {
+    console.error(err);
+    res.json({
+      success: false,
+      error: err.toString()
+    });
+    }
+})
+
+// PUT ROUTE
+router.put('/update-one/:id', (req, res) => {
+    try {
+        const id = req.params.id;
+        /* VALIDATION CHECK */
+        const validateData = () => {
+            if (req.body.isComplete === undefined){
+                res.json({
+                    success: false,
+                    message: "isComplete is a required field."
+                })
+                return;
+            } else if (req.body.isComplete === true){
+                db().collection("toDos").update({
+                    id: id
+                }, {
+                    $set: {
+                        isComplete: true,
+                        completedDate: new Date()
+                    },
+                }
+                )  
+            } else if (req.body.isComplete === false){
+                db().collection("toDos").update({
+                    id: id
+                }, {
+                    $set: {
+                        isComplete: false,
+                        completedDate: null
+                    },
+                }
+                )
+            }
+        };
+        validateData();
+        res.json ({
+            success: true
+          });
+    } catch(err) {
+        console.error(err);
+        res.json({
+          success: false,
+          error: err.toString()
+        });
+    }
+});
+
+router.delete('/delete-one/:id', (req, res) => {
+    try {
+        const id = req.params.id;
+        db().collection("toDos").deleteOne({
+            id: id
+        });
+        
+        res.json ({
+            success: true
+          });
+
+    } catch (err){
+        console.error(err);
+        res.json({
+          success: false,
+          error: err.toString()
+        });
+    }
+})
+module.exports = router;
